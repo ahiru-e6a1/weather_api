9d141505972f96db61f00f7bc20e88ed18572a1d
ahiru-e6a1 <a.h.i.r.u@icloud.com>
2021-07-25 22:04:19 +0900

    Reactの環境構築

---
+++ src/screens/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div>
+      <header className="App-header">
+        <h2>ははは</h2>
+      </header>
+    </div>
+  );
+};
+
+export default Main;
